dd4feac0184db2db9aa0522d25a43dabb41cbb9b
WajdButterfly <wajd2828@gmail.com>
2024-12-11 15:45:33 +0300

    Initial Commit

---
+++ SignLangApp1/SignLangApp1App.swift
@@ -0,0 +1,17 @@
+//
+//  SignLangApp1App.swift
+//  SignLangApp1
+//
+//  Created by Wajd on 11/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct SignLangApp1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
